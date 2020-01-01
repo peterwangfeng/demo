e31ad392c37be0423355eb1a7e3d7a4e4a5f104d
wangfeng <peternodejs@gmail.com>
2020-01-01 22:29:44 +0800

    second

--- src/main/java/com/example/demo/dao/IUserDao.java
+++ src/main/java/com/example/demo/dao/IUserDao.java
@@ -1,11 +1,9 @@
 package com.example.demo.dao;
 
 import com.example.demo.domain.User;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
-@Mapper
 public interface IUserDao {
     List<User> getList();
 }
